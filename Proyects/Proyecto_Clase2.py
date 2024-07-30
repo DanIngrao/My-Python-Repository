e56c0f86fabc0cc512533f17238321e953392158
DanIngrao <daningrao99@gmail.com>
2024-07-30 10:33:10 -0300

    Commit

--- Proyects/Proyecto_Clase2.py
+++
@@ -1,6 +0,0 @@
-#Calculadora de comisiones
-
-nombre= input("Ingrese su nombre completo: ")
-ingresos=float(input("Por favor, coloque los ingresos totales de este mes: $"))
-comision=ingresos*13/100
-print(f"Perfecto {nombre},\nde los ingresos totales de este mes que son ${ingresos} te corresponde por comision del 13% un total de ${round(comision,2)}")
