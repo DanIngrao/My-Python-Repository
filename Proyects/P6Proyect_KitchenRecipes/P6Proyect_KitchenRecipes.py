73150d0a927f0ade2a35bc2b3085bba453ad8353
DanIngrao <daningrao99@gmail.com>
2024-07-30 15:59:01 -0300

    Commit

--- Proyects/P6Proyect_KitchenRecipes/P6Proyect_KitchenRecipes.py
+++ Proyects/P6Proyect_KitchenRecipes/P6Proyect_KitchenRecipes.py
@@ -16,15 +16,23 @@ def panel_menu():
             system('cls')
             break
         elif option == 1:
-            pass
+            category = select_category()
+            if category != 1:
+                select_recipe(category)
         elif option == 2:
             pass
         elif option == 3:
             pass
         elif option == 4:
-            pass
+            category = select_category()
+            if category != 1:
+                recipe = select_recipe(category)
+                if recipe != 1:
+                    delete_recipe(recipe)
         elif option == 5:
-            pass
+            category = select_category()
+            if category != 1:
+                delete_category(category)
         else:
             print('Error, invalid option. Try again')
 
@@ -34,26 +42,36 @@ def select_category():
         option = int(input(f'Select category below:\n[0] - Back\n{'\n'.join([f'[{categories_list.index(r)+1}] - {r}' for r in categories_list])}\n'))
         if option == 0:
             system('cls')
-            break
+            return 1
         elif option in range(1,len(categories_list) + 1):
             category = Path(f'{route}\\{categories_list[option-1]}')
             return category
         else:
             print('Error, invalid option. Try again')
 
-def select_recipe(category):
+def select_recipe(category):    
     system('cls')
     recipes_list = list(category.glob('**/*'))
     option = int(input(f'Select receipe below:\n[0] - Back\n{'\n'.join([f'[{recipes_list.index(r)+1}] - {r.stem}' for r in recipes_list])}\n'))
     while True:
         if option == 0:
             system('cls')
-            break
+            return 1
         elif option in range(1,len(recipes_list)+1):
             recipe = Path(f'{recipes_list[option-1]}')
             print(recipe.read_text())
             return recipe
         else:
             print('Error, invalid option. Try again')
+
+def delete_category(category):
+    os.rmdir(category)
+    print(f'Category "{category.stem}" removed')
+
+def delete_recipe(recipe):
+    os.remove(recipe)
+    print(f'Recipe "{recipe.stem}" removed')
+
+
 
 panel_menu()
