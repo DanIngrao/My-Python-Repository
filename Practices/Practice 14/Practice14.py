508944dae92ee833954ca91be0053344907c3e9f
DanIngrao <daningrao99@gmail.com>
2024-08-15 16:13:28 -0300

    P14Proyect_AssistanceDriver

--- Practices/Practice 14/Practice14.py
+++ Practices/Practice 14/Practice14.py
@@ -3,19 +3,40 @@ import face_recognition as fr
 import numpy as np
 
 # Cargar imagenes
-foto_control = cv2.imread('Practices\\Practice 14\\FotoA.jpg')
-foto_prueba = cv2.imread('Practices\\Practice 14\\FotoB.jpg')
-
-# Mostrar imagenes
-cv2.imshow('Foto Control', foto_control)
-cv2.imshow('Foto Prueba', foto_prueba)
+foto_control = fr.load_image_file('Practices\\Practice 14\\FotoA.jpg')
+foto_prueba = fr.load_image_file('Practices\\Practice 14\\FotoC.jpg')
 
 # Pasar imagenes a RGB
 foto_control = cv2.cvtColor(foto_control, cv2.COLOR_BGR2RGB)
 foto_prueba = cv2.cvtColor(foto_prueba, cv2.COLOR_BGR2RGB)
 
 # Localizar cara control
-cara_control = fr.face_locations(foto_control)
+lugar_cara_A = fr.face_locations(foto_control)[0]
+cara_codificada_A = fr.face_encodings(foto_control)[0]
+print(lugar_cara_A)
+
+lugar_cara_B = fr.face_locations(foto_prueba)[0]
+cara_codificada_B = fr.face_encodings(foto_prueba)[0]
+print(lugar_cara_B)
+
+# Mostrar rectangulos
+cv2.rectangle(foto_control, (lugar_cara_A[3],lugar_cara_A[0]),(lugar_cara_A[1],lugar_cara_A[2]),(0,0,255),3)
+cv2.rectangle(foto_prueba, (lugar_cara_B[3],lugar_cara_B[0]),(lugar_cara_B[1],lugar_cara_B[2]),(0,0,255),3)
+
+# Realizar comparacion
+resultado = fr.compare_faces([cara_codificada_A], cara_codificada_B)
+print(resultado)
+
+# Medida de la distancia
+distancia = fr.face_distance([cara_codificada_A],cara_codificada_B)
+
+# Mostrar resultado
+cv2.putText(foto_prueba,f'{resultado}{distancia.round(2)}',(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),3)
+print(distancia)
+
+# Mostrar imagenes
+cv2.imshow('Foto Control', foto_control)
+cv2.imshow('Foto Prueba', foto_prueba)
 
 # Mantener programa abierto
 cv2.waitKey(0)
---
+++ Proyects/P14Proyect_AssistanceDriver/P14Proyect_AssistanceDriver.py
@@ -0,0 +1,68 @@
+import os
+import cv2
+import face_recognition as fr
+import numpy as np
+
+# Data Base
+
+dir_employees = 'Proyects\P14Proyect_AssistanceDriver\Employees'
+
+employees_list = os.listdir(dir_employees)
+
+db_employees = []
+
+print(employees_list)
+
+for name in employees_list:
+    
+    actual_image = cv2.imread(f'{dir_employees}\\{name}')
+
+    actual_image_cvt = cv2.cvtColor(actual_image, cv2.COLOR_BGR2RGB)
+    
+    actual_image_encoded = fr.face_encodings(actual_image_cvt)[0]
+    
+    db_employees.append(dict([('name',os.path.splitext(name)[0]),('img_code',actual_image_encoded),('img',actual_image),('assist_log',0)]))
+
+print([f'{emp['name'],emp['assist_log']}' for emp in db_employees])
+
+# Function
+
+def face_search():
+
+    face_in_cam = cv2.imread('Proyects\\P14Proyect_AssistanceDriver\\Employees\\Brad Pitt.jpg')
+
+    face_in_cam_cvt = cv2.cvtColor(face_in_cam, cv2.COLOR_BGR2RGB)
+    
+    face_map = fr.face_locations(face_in_cam_cvt)[0]
+
+    face_encoded = fr.face_encodings(face_in_cam_cvt)[0]
+
+    cv2.rectangle(face_in_cam, (face_map[3],face_map[0]),(face_map[1],face_map[2]),(0,255,0),3)
+    
+    for emp in db_employees:
+    
+        if fr.compare_faces([emp['img_code']], face_encoded) == np.True_:
+    
+            print(f'Employee: {emp["name"]}')
+    
+            emp['assist_log'] += 1
+            
+            match_value = fr.face_distance([face_encoded],emp['img_code'])
+
+            cv2.putText(face_in_cam,f'Match: {match_value*100}%',(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+            
+            cv2.imshow('Data Base Match',emp['img'])
+            cv2.imshow('Face', face_in_cam)
+
+    for emp in db_employees:
+        print(
+f'''
+Name: {emp['name']}
+Assistance: {emp['assist_log']}
+*******************************
+'''
+        )
+
+face_search()
+
+cv2.waitKey(0)
